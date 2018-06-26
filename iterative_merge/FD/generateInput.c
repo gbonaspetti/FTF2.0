d9181d75009f07eba903a7c92bff572c260936e0
Giulia Bonaspetti <vini_f_netto@bitbucket.com>
2018-06-26 10:12:11 -0300

    novo tamanho do iterative

--- iterative_merge/FD/generateInput.c
+++ iterative_merge/FD/generateInput.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INPUTSIZE 100000
+#define INPUTSIZE 1000000
 int main(){
 
         unsigned *ndata = (unsigned*)malloc(INPUTSIZE*sizeof(unsigned));//new unsigned[INPUTSIZE];
